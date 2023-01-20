394640edd5043e09d0dcf88fc7de767de3b6ed9f
Joan C <joan0492@gmail.com>
2023-01-20 03:05:53 -0500

    410. Axios - Configurar cliente para peticiones HTTP

---
+++ src/shared/api/calendarApi.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { getEnvVariables } from "@/shared/helpers";
+
+const { VITE_API_URL } = getEnvVariables();
+
+const calendarApi = axios.create({
+  baseURL: VITE_API_URL,
+});
+
+// Todo: Configurar interceptores
+
+export default calendarApi;
+
+
