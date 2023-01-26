fb649b1c6d0fa47da16483765b1e0a412d9e15bc
Joan C <joan0492@gmail.com>
2023-01-26 11:21:43 -0500

    up in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 // import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { App } from "./App";
 import "@/assets/css/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
+  // </BrowserRouter>
   // </React.StrictMode>
 );
